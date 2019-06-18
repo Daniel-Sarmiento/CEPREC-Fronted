f8a7ea96434deb75670d8430c7f25f2a584bf678
DanielSarmiento <163233@ids.upchiapas.edu.mx>
2019-06-18 16:53:14 -0500

    Se crearon los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearcherComponent },
+  { path: 'buscador', component: SearcherComponent },
+  { path: 'estadisticas', component: StatsComponent },
+  { path: 'acerca-de', component: AboutComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearcherComponent } from './components/searcher/searcher.component';
+import { StatsComponent } from './components/stats/stats.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearcherComponent,
+    StatsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
