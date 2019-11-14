dbffc3b48d1e2619ede20df7b4380cfbde6d0fc1
DanielSarmiento <163233@ids.upchiapas.edu.mx>
2019-11-13 19:42:36 -0600

    se corrigió la ruta del dist

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/CEPREC-Fronted'));
+app.use(express.static(__dirname+'/dist/buscadorFrontAngular'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/CEPREC-Fronted/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/buscadorFrontAngular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
